2bdf9c7b3316bd3667a79be3ff1b9eef33fcdfcd
Aarooshismartzone <127930685+Aarooshismartzone@users.noreply.github.com>
2024-03-16 11:11:39 +0530

    Creating Fresh Project in Next JS

---
+++ src/app/ecommerce-website/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function page() {
+    return (
+        <>
+            <div className="bg-gradient-to-r from-violet-900 to-fuchsia-900 w-full h-full">
+                <h1 className='text-white'>Hi</h1>
+            </div>
+        </>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,95 @@
-import Image from "next/image";
+"use client"
+import React, { useState } from 'react'
+import { BsInfoSquareFill } from "react-icons/bs";
+import { CgWebsite } from "react-icons/cg";
+import { FaBlog } from "react-icons/fa";
+import { GiLetterBomb, GiShoppingCart } from "react-icons/gi";
+import { IoReader } from "react-icons/io5";
+import { MdOutlineAppShortcut, MdOutlineWeb } from "react-icons/md";
+import { RiFileInfoFill } from "react-icons/ri";
 
 export default function Home() {
+
+  const [value, newValue] = useState({
+    //put all the classes of the elements where classes will change. Include even those classes that won't change
+    cls1: 'h-4 w-4 bg-gray-600',
+    cls2: 'h-4 w-4 bg-gray-600',
+    cls3: 'h-4 w-4 bg-gray-600',
+    cls4: 'h-4 w-4 bg-gray-600',
+    cls5: 'h-4 w-4 bg-gray-600',
+    cls6: 'h-4 w-4 bg-gray-600',
+    cls7: 'h-4 w-4 bg-gray-600',
+    cls8: 'h-4 w-4 bg-gray-600',
+    cls9: 'h-4 w-4 bg-gray-600',
+    ocs: 'flex gap-4 justify-between p-2',
+    box: 'relative bg-black opacity-60 h-24 w-24 align-center rounded-lg shadow-lg shadow-cyan-500/50',
+    linkCl: 'hidden'
+  });
+
+  const cls1 = value.cls1
+  const cls2 = value.cls2
+  const cls3 = value.cls3
+  const cls4 = value.cls4
+  const cls5 = value.cls5
+  const cls6 = value.cls6
+  const cls7 = value.cls7
+  const cls8 = value.cls8
+  const cls9 = value.cls9
+  const ocs = value.ocs
+  const box = value.box
+  const linkCl = value.linkCl
+
+  function expand() {
+    newValue(a => {
+      return {
+        //put the new classes here. Also include those classes that won't change - as the entire class property will get replaced.
+        ...a,
+        cls1: 'md:h-24 md:w-24 w-16 h-16 md:bg-gray-300 hover:bg-gray-100 bg-transparent duration-400 p-1 clss1',
+        cls2: 'md:h-24 md:w-24 w-16 h-16 md:bg-gray-300 hover:bg-gray-100 bg-transparent duration-400 p-1 clss2',
+        cls3: 'md:h-24 md:w-24 w-16 h-16 md:bg-gray-300 hover:bg-gray-100 bg-transparent duration-400 p-1 clss3',
+        cls4: 'md:h-24 md:w-24 w-16 h-16 md:bg-gray-300 hover:bg-gray-100 bg-transparent duration-400 p-1 clss4',
+        cls5: 'md:h-24 md:w-24 w-16 h-16 md:bg-gray-300 hover:bg-gray-100 bg-transparent duration-400 p-1 clss5',
+        cls6: 'md:h-24 md:w-24 w-16 h-16 md:bg-gray-300 hover:bg-gray-100 bg-transparent duration-400 p-1 clss6',
+        cls7: 'md:h-24 md:w-24 w-16 h-16 md:bg-gray-300 hover:bg-gray-100 bg-transparent duration-400 p-1 clss7',
+        cls8: 'md:h-24 md:w-24 w-16 h-16 md:bg-gray-300 hover:bg-gray-100 bg-transparent duration-400 p-1 clss8',
+        cls9: 'md:h-24 md:w-24 w-16 h-16 md:bg-gray-300 hover:bg-gray-100 bg-transparent duration-400 p-1 clss9',
+        ocs: 'flex md:gap-4 gap-2 md:p-3 px-1 py-3 md:justify-between justify-evenly',
+        box: 'relative flex flex-col justify-between align-middle bg-black py-2 opacity-60 w-72 md:w-96 md:w-96 rounded-lg shadow-lg shadow-cyan-500/50 duration-200',
+        linkCl: 'block w-full h-full linkcl duration-500 md:text-sm txs text-white md:text-gray-900 text-center font-bold'
+      }
+    })
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+        <div className="bg-gradient-to-r from-violet-900 to-fuchsia-900 w-full h-full">
+          <div className="absolute left-4 top-4">
+            <img src="images/logo.png" className="w-52" />
+            <div className="sm:block hidden">
+              <h1 className="text-3xl text-white mt-2 mx-5">Take your business online. We are there for you.</h1>
+              <p className="text-white font-bold mt-2 mx-5">Explore our services. Click the box below.</p>
+            </div>
+          </div>
+          <div className="flex items-center justify-center h-screen">
+            <div className={box} onClick={expand}>
+              <div className={ocs}>
+                <div className={cls1}><a href='/' className={linkCl}><GiShoppingCart className="md:my-2 my-1 md:mx-7 mx-4 md:text-3xl text-xl text-white md:text-fuchsia-800" />E-commerce website</a></div>
+                <div className={cls2}><a href='/' className={linkCl}><CgWebsite className="md:my-2 my-1 md:mx-7 mx-4 md:text-3xl text-xl text-white md:text-fuchsia-800" />LMS/CRM portal</a></div>
+                <div className={cls3}><a href='/' className={linkCl}><BsInfoSquareFill className="md:my-2 my-1 md:mx-7 mx-4 md:text-3xl text-xl text-white md:text-fuchsia-800" />Informative website</a></div>
+              </div>
+              <div className={ocs}>
+                <div className={cls4}><a href='/' className={linkCl}><FaBlog className="md:my-2 my-1 md:mx-7 mx-4 md:text-3xl text-xl text-white md:text-fuchsia-800" />Blog/ E-magazine</a></div>
+                <div className={cls5}><a href='/' className={linkCl}><MdOutlineWeb className="md:my-2 my-1 md:mx-7 mx-4 md:text-3xl text-xl text-white md:text-fuchsia-800" />Any Other Website</a></div>
+                <div className={cls6}><a href='/' className={linkCl}><MdOutlineAppShortcut className="md:my-2 my-1 md:mx-7 mx-4 md:text-3xl text-xl text-white md:text-fuchsia-800" />Mobile Application</a></div>
+              </div>
+              <div className={ocs}>
+                <div className={cls7}><a href='/' className={linkCl}><RiFileInfoFill className="md:my-2 my-1 md:mx-7 mx-4 md:text-3xl text-xl text-white md:text-fuchsia-800" />About Aarooshi</a></div>
+                <div className={cls8}><a href='/' className={linkCl}><GiLetterBomb className="md:my-2 my-1 md:mx-7 mx-4 md:text-3xl text-xl text-white md:text-fuchsia-800" />Get In Touch</a></div>
+                <div className={cls9}><a href='/' className={linkCl}><IoReader className="md:my-2 my-1 md:mx-7 mx-4 md:text-3xl text-xl text-white md:text-fuchsia-800" />Aarooshi Blog</a></div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
