59c4645d49c9c58fedae7714d99607fc42161c34
Aarooshismartzone <127930685+Aarooshismartzone@users.noreply.github.com>
2024-04-14 16:48:41 +0530

    adding content from posts

--- src/app/(blog)/blog/page.tsx
+++ src/app/(blog)/blog/page.tsx
@@ -2,17 +2,26 @@
 import React, { useEffect, useState } from 'react'
 import styles from './styles/style.module.scss'
 import BlogTopNav from '@/components/blog/blogtopnav'
+import { PostData } from '@/components/blog/data'
 
 
 export default function page() {
     
+const posts = PostData()
 
     return (
         <>
             <div className='bg-gray-800 text-white h-lvh'>
             <BlogTopNav />
                 <Anim />
-                <h2 className='text-xl mt-4 text-center'>Fresh posts coming soon. Sit tight!</h2>
+                {posts.map((p) => {
+                    return (
+                        <>
+                        {p.title}<br/>
+                        {p.content}
+                        </>
+                    )
+                })}
             </div>
         </>
     )
