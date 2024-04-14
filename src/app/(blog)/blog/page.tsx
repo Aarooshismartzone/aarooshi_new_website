2b056a00cef5f299ff1d17ff922903070fe04f9a
Aarooshismartzone <127930685+Aarooshismartzone@users.noreply.github.com>
2024-04-14 17:42:27 +0530

    Update page.tsx

--- src/app/(blog)/blog/page.tsx
+++ src/app/(blog)/blog/page.tsx
@@ -15,31 +15,33 @@ export default function page() {
             <div className='bg-gray-800 text-white min-h-lvh'>
                 <BlogTopNav />
                 <Anim />
-                <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-3'>
-                {posts.map((p) => {
-                    return (
-                        <>
-                            <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-7">
-                                <CardHeader className="absolute z-10 top-1 flex-col items-start">
+                <div className='grid grid-col lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-3'>
+                    {posts.map((p) => {
+                        return (
+                            <>
+                                <div className='col-span-1'>
+                                    <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-7">
+                                        <CardHeader className="absolute z-10 top-1 flex-col items-start">
 
-                                </CardHeader>
-                                <Image
-                                    removeWrapper
-                                    alt="Relaxing app background"
-                                    className="z-0 w-full h-full object-cover"
-                                    src={"/images/blog/" + p.image}
-                                />
-                                <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
+                                        </CardHeader>
+                                        <Image
+                                            removeWrapper
+                                            alt="Relaxing app background"
+                                            className="z-0 w-full h-full object-cover"
+                                            src={"/images/blog/" + p.image}
+                                        />
+                                        <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
 
-                                        <div className="flex flex-col">
-                                            <p className="text-tiny text-white/60 uppercase font-bold">{p.readable_mins}-MINUTE READ</p>
-                                            <h4 className="text-white/90 font-medium text-xl">{p.title}</h4>
-                                        </div>
-                                </CardFooter>
-                            </Card>
-                        </>
-                    )
-                })}
+                                            <div className="flex flex-col">
+                                                <p className="text-tiny text-white/60 uppercase font-bold">{p.readable_mins}-MINUTE READ</p>
+                                                <h4 className="text-white/90 font-medium text-xl">{p.title}</h4>
+                                            </div>
+                                        </CardFooter>
+                                    </Card>
+                                </div>
+                            </>
+                        )
+                    })}
                 </div>
             </div>
         </>
