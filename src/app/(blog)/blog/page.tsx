e923604947a415093f83c05c71292981a6cc6937
Aarooshismartzone <127930685+Aarooshismartzone@users.noreply.github.com>
2024-04-14 18:08:38 +0530

    Update page.tsx

--- src/app/(blog)/blog/page.tsx
+++ src/app/(blog)/blog/page.tsx
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from 'react'
 import styles from './styles/style.module.scss'
 import BlogTopNav from '@/components/blog/blogtopnav'
 import { PostData } from '@/components/blog/data'
-import { Card, CardHeader, CardBody, CardFooter, Image, Button } from "@nextui-org/react";
+import { Card, CardFooter, Image } from "@nextui-org/react";
+import Link from 'next/link'
 
 
 export default function page() {
@@ -20,24 +21,24 @@ export default function page() {
                         return (
                             <>
                                 <div className='col-span-1'>
-                                    <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-7">
-                                        <CardHeader className="absolute z-10 top-1 flex-col items-start">
+                                    <Link href={'/post/' + p.slug}>
+                                        <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-7">
+                                            <Image
+                                                removeWrapper
+                                                isZoomed
+                                                alt="Aarooshi"
+                                                className="z-0 w-full h-full object-cover"
+                                                src={"/images/blog/" + p.image}
+                                            />
+                                            <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
 
-                                        </CardHeader>
-                                        <Image
-                                            removeWrapper
-                                            alt="Relaxing app background"
-                                            className="z-0 w-full h-full object-cover"
-                                            src={"/images/blog/" + p.image}
-                                        />
-                                        <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
-
-                                            <div className="flex flex-col">
-                                                <p className="text-tiny text-white/60 uppercase font-bold">{p.readable_mins}-MINUTE READ</p>
-                                                <h4 className="text-white/90 font-medium text-xl">{p.title}</h4>
-                                            </div>
-                                        </CardFooter>
-                                    </Card>
+                                                <div className="flex flex-col">
+                                                    <p className="text-tiny text-white/60 uppercase font-bold">{p.readable_mins}-MINUTE READ</p>
+                                                    <h4 className="text-white/90 font-medium text-xl">{p.title}</h4>
+                                                </div>
+                                            </CardFooter>
+                                        </Card>
+                                    </Link>
                                 </div>
                             </>
                         )
