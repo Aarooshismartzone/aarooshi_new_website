9b6732d26998b4792128b52ba80080ac3202838a
Aarooshismartzone <127930685+Aarooshismartzone@users.noreply.github.com>
2024-04-16 22:50:01 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,7 +75,7 @@ export default function Home() {
     <>
       <div className="bg-gradient-to-r from-violet-900 to-fuchsia-900 w-full h-full">
         <div className="absolute left-4 top-4">
-          <Image src="images/logo.png" className='w-52' alt='Aarooshi' />
+          <Image src="images/logo.png" className='w-52' width={208} height={20} alt='Aarooshi' />
           <div className="sm:block hidden">
             <h1 className="text-3xl text-white mt-2">Take your business online. We are there for you.</h1>
             <p className={info}>Explore our services. Click the box at the centre.</p>
