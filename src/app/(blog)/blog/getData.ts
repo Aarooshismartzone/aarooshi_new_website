9371cb8c099a80013edab43a15ff2b35f05e1ed9
Aarooshismartzone <127930685+Aarooshismartzone@users.noreply.github.com>
2024-06-20 20:51:10 +0530

    Update getData.ts

--- src/app/(blog)/blog/getData.ts
+++ src/app/(blog)/blog/getData.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+//import type { NextApiRequest, NextApiResponse } from 'next';
 import clientPromise from '../../../lib/mongodb';
 
 export interface CategoryType {
@@ -28,11 +28,11 @@ export const MdbCategories = async () => {
     const db = client.db('aarooshi'); // database name
     const collection = db.collection('categories'); // collection name
     const data = await collection.find({}).toArray();
-    
+
     const categories: CategoryType[] = data.map((category: any) => ({
       _id: category._id.toString(), // Assuming _id is of type ObjectId and needs to be converted to string
       slug: category.slug,
-      name: category.name,     
+      name: category.name,
     }));
 
     return categories;
@@ -42,31 +42,32 @@ export const MdbCategories = async () => {
   }
 };
 
-export const MdbPosts = async (req: NextApiRequest, res: NextApiResponse) => {
-    try {
-      const client = await clientPromise;
-      const db = client.db('aarooshi'); // database name
-      const collection = db.collection('posts'); // collection name
-      const data = await collection.find({}).toArray();
-  
-      //res.status(200).json(data);
-      const posts: MDPostType[] = data.map((post:any) => ({
-        _id: post._id.toString(),
-        cat_id: post.cat_id,
-        cat_name: post.cat_name,
-        title: post.title,
-        slug: post.slug,
-        content: post.content,
-        image: post.image,
-        tags: post.tags,
-        keywords: post.keywords,
-        description: post.description,
-        readable_mins: post.readable_mins,
-        date: post.date,
-      }))
-    } catch (e) {
-      console.error(e);
-     // res.status(500).json({ error: 'Unable to fetch data' });
-     throw new Error('Unable to fetch data');
-    }
-  };
+// export const MdbPosts = async (req: NextApiRequest, res: NextApiResponse) => {
+export const MdbPosts = async () => {
+  try {
+    const client = await clientPromise;
+    const db = client.db('aarooshi'); // database name
+    const collection = db.collection('posts'); // collection name
+    const data = await collection.find({}).toArray();
+
+    //res.status(200).json(data);
+    const posts: MDPostType[] = data.map((post: any) => ({
+      _id: post._id.toString(),
+      cat_id: post.cat_id,
+      cat_name: post.cat_name,
+      title: post.title,
+      slug: post.slug,
+      content: post.content,
+      image: post.image,
+      tags: post.tags,
+      keywords: post.keywords,
+      description: post.description,
+      readable_mins: post.readable_mins,
+      date: post.date,
+    }))
+  } catch (e) {
+    console.error(e);
+    // res.status(500).json({ error: 'Unable to fetch data' });
+    throw new Error('Unable to fetch data');
+  }
+};
