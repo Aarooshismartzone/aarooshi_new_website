89a2b604726cb8655b1c696b0b5289b921cc6d4d
Aarooshismartzone <127930685+Aarooshismartzone@users.noreply.github.com>
2024-11-27 01:34:52 +0530

    Update Page.tsx

--- src/app/privacy-policy/Page.tsx
+++ src/app/privacy-policy/Page.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './style.scss'
 import PrivacyPolicy from '@/components/privacy-policy';
 import Topbar from '@/components/topbar';
 import Topiclist from '@/components/topiclist';
@@ -7,16 +6,15 @@ import Topiclist from '@/components/topiclist';
 export default function Page() {
   return (
     <>
-      <main>
-        <Topbar name="Privacy Policy" />
-        <div className='belowtopbar'>
-          <Topiclist />
-          <PrivacyPolicy />
-        </div>
-        <div className='md:hidden block abovetopbar'>
-          <PrivacyPolicy />
-        </div>
-      </main>
+      {/* <Topbar name="Privacy Policy" />
+      <div className='belowtopbar'>
+        <Topiclist />
+        <PrivacyPolicy />
+      </div>
+      <div className='md:hidden block abovetopbar'>
+        <PrivacyPolicy />
+      </div> */}
+      <p>Privacy Policy</p>
     </>
   );
 }
