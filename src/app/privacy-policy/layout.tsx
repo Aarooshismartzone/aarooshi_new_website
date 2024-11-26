0748cbb62c79f6e3b5371d4329564ba909c9cef0
Aarooshismartzone <127930685+Aarooshismartzone@users.noreply.github.com>
2024-11-27 01:23:06 +0530

    PP changes done

---
+++ src/app/privacy-policy/layout.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+import Page from "./page";
+
+
+export const metadata: Metadata = {
+  title: 'Privacy Policy',
+  description:
+    "Need a website and mobile app? Get it from us.",
+};
+export default Page;
