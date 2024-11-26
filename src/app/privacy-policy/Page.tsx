90cbc7c61a4373951677d8a566850f43d3e5d2ec
Aarooshismartzone <127930685+Aarooshismartzone@users.noreply.github.com>
2024-11-27 01:25:59 +0530

    Update Page.tsx

--- src/app/privacy-policy/Page.tsx
+++ src/app/privacy-policy/Page.tsx
@@ -7,16 +7,16 @@ import Topiclist from '@/components/topiclist';
 export default function Page() {
   return (
     <>
-    <main>
-                <Topbar name="Privacy Policy" />
-                <div className='belowtopbar'>
-                    <Topiclist />
-                    <PrivacyPolicy/>
-                </div>
-                <div className='md:hidden block abovetopbar'>
-                    <PrivacyPolicy/>
-                </div>
-            </main>
+      <main>
+        <Topbar name="Privacy Policy" />
+        <div className='belowtopbar'>
+          <Topiclist />
+          <PrivacyPolicy />
+        </div>
+        <div className='md:hidden block abovetopbar'>
+          <PrivacyPolicy />
+        </div>
+      </main>
     </>
   );
 }
