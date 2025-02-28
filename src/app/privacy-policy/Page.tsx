809dba5b5ab766b22d449709444e7789f6aef6d7
Aarooshismartzone <127930685+Aarooshismartzone@users.noreply.github.com>
2025-02-28 21:14:47 +0530

    changes made in privacy policy design

--- src/app/privacy-policy/Page.tsx
+++ src/app/privacy-policy/Page.tsx
@@ -2,20 +2,21 @@ import React from 'react';
 import PrivacyPolicy from '@/components/privacy-policy';
 import Topbar from '@/components/topbar';
 import Topiclist from '@/components/topiclist';
+import './style.scss';
 
 export default function Page() {
   return (
     <>
-      {/* <Topbar name="Privacy Policy" />
+      <Topbar name="Privacy Policy" />
       <div className='belowtopbar'>
         <Topiclist />
         <PrivacyPolicy />
       </div>
       <div className='md:hidden block abovetopbar'>
         <PrivacyPolicy />
-      </div> */}
-      <p>Privacy Policy</p>
-      <p>Privacy policy comes here</p>
+      </div>
+      {/* <p>Privacy Policy</p>
+      <p>Privacy policy comes here</p> */}
     </>
   );
 }
