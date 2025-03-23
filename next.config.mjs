1f4a70ecf59b92b030c53797590b91e4f1186929
Aarooshismartzone <127930685+Aarooshismartzone@users.noreply.github.com>
2025-03-23 11:33:44 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+          {
+            source: "/(.*)",
+            has: [{ type: "protocol", value: "http" }],
+            destination: "https://aarooshi.com/:path*",
+            permanent: true,
+          },
+        ];
+      },
+};
 
 export default nextConfig;
